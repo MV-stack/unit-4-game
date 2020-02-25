93fea4a944814870df61df94998b086615c2f8f1
MV-stack <59325046+MV-stack@users.noreply.github.com>
2020-02-25 13:09:19 -0700

    added javascript arrays for random numbers, adjusted column widths in html

---
+++ assets/javascript/game.js
@@ -0,0 +1,26 @@
+$(document).ready(function () {
+
+            //random numbers array
+            var randNum = [];
+            for (var n = 19; n < 121; n++) {
+                randNum.push(n);
+            }
+            console.log(randNum);
+
+            //random crystals array
+
+            var crystals = [];
+            for (var c = 1; c < 13; c++) {
+                crystals.push(c);
+            }
+            console.log(crystals);
+
+            var matchNum; //number to match
+            var crystalNum = []; //contains crystal values
+
+            var crystal1;
+            var crystal2;
+            var crystal3;
+            var crystal4;
+
+        }
