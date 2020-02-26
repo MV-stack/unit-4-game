b392e0af013859043da30649301cd1f54bbc6b40
MV-stack <59325046+MV-stack@users.noreply.github.com>
2020-02-26 07:33:16 -0700

    worked on javascript/jquery functions

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,26 +1,47 @@
 $(document).ready(function () {
+    // Initialize variables for score, wins, losses
+    var score = 0;
+    var losses = 0;
+    var wins = 0;
+    // Generate number to match and display 
+    var randomNumber = randomNumberGen(19, 120);
+    $("#random-number").text(randomNumber)
 
-            //random numbers array
-            var randNum = [];
-            for (var n = 19; n < 121; n++) {
-                randNum.push(n);
-            }
-            console.log(randNum);
+    function randomNumberGen(min, max) {
+        return Math.floor(Math.random() * max) + min;
+    }
+    
+    $(".gems").on("click", function () {
+        crystalValue = parseInt($(this).val());
+        console.log("crystal value : " + crystalValue);
+        score = score + crystalValue;
+        console.log("score: " + score);
+        $("#score").text(score)
+        if (score == randomNumber) {
+            wins++;
+            alert("You Won")
+            $("#wins").text(wins)
+            restartGame();
+        } else if (score > randomNumber) {
+            losses++;
+            alert("You Lost ! Try again");
+            $("#losses").text(losses);
+            restartGame()
+        }
+    })
 
-            //random crystals array
-
-            var crystals = [];
-            for (var c = 1; c < 13; c++) {
-                crystals.push(c);
-            }
-            console.log(crystals);
-
-            var matchNum; //number to match
-            var crystalNum = []; //contains crystal values
-
-            var crystal1;
-            var crystal2;
-            var crystal3;
-            var crystal4;
+    function restartGame() {
+        randomNumber = randomNumberGen(1, 100);
+        $("#random-number").text(randomNumber)
+        score = 0;
+        $("#score").text(score)
+        for (let i = 0; i < $(".gems").length; i++) {
+            var gemValue = randomNumberGen(1, 20);
+            $("#b-" + i).val(gemValue)
 
         }
+    }
+    restartGame();
+
+
+})
